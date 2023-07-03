46c4599cf11880c5e29324d5eb61f23b2705e4f3
MetaBrian <114273788+MetaBrian@users.noreply.github.com>
2023-07-03 07:13:11 -0400

    added preliminary routes with preliminary components & error routing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { DeckBuilderComponent } from './deck-builder/deck-builder.component';
+import { CardRaterComponent } from './card-rater/card-rater.component';
+
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ErrorComponent } from './error/error.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'cardrater', component: CardRaterComponent },
+  { path: 'deckbuilder', component: DeckBuilderComponent },
+  { path: '**', component: ErrorComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
 
-    NavBarComponent
+    NavBarComponent,
+    DeckBuilderComponent,
+    CardRaterComponent,
+    HomeComponent,
+    ErrorComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/deck-builder/deck-builder.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deck-builder',
+  templateUrl: './deck-builder.component.html',
+  styleUrls: ['./deck-builder.component.css']
+})
+export class DeckBuilderComponent {
+
+}
