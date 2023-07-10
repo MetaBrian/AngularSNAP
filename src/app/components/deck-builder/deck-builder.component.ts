d3ef60495429eded25997a94eabae671bce5137c
MetaBrian <114273788+MetaBrian@users.noreply.github.com>
2023-07-10 19:32:26 -0400

    updated styling of images used within the app

--- src/app/components/deck-builder/deck-builder.component.ts
+++ src/app/components/deck-builder/deck-builder.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './deck-builder.component.html',
   styleUrls: ['./deck-builder.component.css'],
 })
+
 export class DeckBuilderComponent {
   //pass in the button's name which will be made from the API
   addCard(cardName: string) {
