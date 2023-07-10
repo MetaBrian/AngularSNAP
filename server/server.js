4aadcaae2804357d0a632703b7b0583d5be7471b
MetaBrian <114273788+MetaBrian@users.noreply.github.com>
2023-07-10 11:43:37 -0400

    added server error handler & server 7120 as well as a script to run the back-end & front-end simultaneously

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
-const dotenv = require('dotenv/config')
+const dotenv = require("dotenv/config");
 const mongoose = require("mongoose");
+const app = express();
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json());
 
 const URI = process.env.URI;
 
@@ -12,3 +17,19 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+
+  app.use('*', (err, res) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
+
+  app.listen(7120, () => {
+  console.log(`Server is running on port 7120.`);
+});
